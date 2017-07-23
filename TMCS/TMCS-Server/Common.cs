bffbd42d806fe9aa921e4bd38abf6e9679aa95c1
Dwscdv3 <dwscdv3@hotmail.com>
2017-07-24 02:14:32 +0800

    看draft

---
+++ TMCS/TMCS-Server/Common.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace TMCS.Server
+{
+    public static class Common
+    {
+        public static bool HasProperty(this object obj, string propName)
+        {
+            return obj.GetType().GetProperties().Any(prop => prop.Name.Equals(propName));
+        }
+    }
+}
